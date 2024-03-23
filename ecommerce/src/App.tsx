32e9496849e05a42a700f42c78761572811fd2c0
Jenny Pieloor <jennypieloor.connect@gmail.com>
2024-03-24 02:21:53 +0800

    almost done

--- ecommerce/src/App.tsx
+++ ecommerce/src/App.tsx
@@ -4,7 +4,10 @@ import { IoFastFoodSharp } from "react-icons/io5";
 import { CiSearch } from "react-icons/ci";
 import { LuShoppingCart } from "react-icons/lu";
 import { IoMdHeartEmpty } from "react-icons/io";
-import { FaRegUser } from "react-icons/fa";
+import { FaMinus, FaOpencart, FaPlus, FaRegUser, FaShoppingBasket } from "react-icons/fa";
+import { FiMinus } from "react-icons/fi";
+import { BsCart4 } from "react-icons/bs";
+import { FaBasketShopping } from "react-icons/fa6";
 
 function App() {
   return (
@@ -28,7 +31,7 @@ function App() {
           </div>
 
           <div className="flex flex-row justify-center items-center gap-3">
-            <button className="flex flex-row justify-center items-center gap-[8px] bg-white py-[10px] w-[120px] h-[44px] rounded-[8px]">
+            <button className="flex flex-row justify-center items-center gap-[8px] bg-white py-[10px] w-[120px] h-[44px] rounded-[8px] hover:scale-95 transition-all hover:bg-[#F48E28] hover:text-white">
               <LuShoppingCart />
               <p className="text-[12px] font-medium">Cart</p>
               <p className="text-[10px] font-medium text-white bg-[#F48E28] px-[6px] py-[2px] rounded-full">
@@ -36,27 +39,35 @@ function App() {
               </p>
             </button>
 
-            <button className="flex flex-row justify-center items-center gap-[8px] bg-white py-[10px] w-[44px] h-[44px] rounded-[8px]">
+            <button className="flex flex-row justify-center items-center gap-[8px] bg-white py-[10px] w-[44px] h-[44px] rounded-[8px] hover:scale-95 transition-all hover:bg-[#F48E28] hover:text-white">
               <IoMdHeartEmpty />
             </button>
 
-            <button className="flex flex-row justify-center items-center gap-[8px] bg-white py-[10px] w-[44px] h-[44px] rounded-[8px]">
+            <button className="flex flex-row justify-center items-center gap-[8px] bg-white py-[10px] w-[44px] h-[44px] rounded-[8px] hover:scale-95 transition-all hover:bg-[#F48E28] hover:text-white">
               <FaRegUser />
             </button>
           </div>
         </div>
 
         {/*Sub-Navbar*/}
-        <div className="flex flex-row justify-between items-center px-60 p-5 border-b-[1px] text-[14px]">
-          <div className="flex flex-row justify-center items-center gap-5 border-[1px] border-[#E1E3E5] rounded-[4px] py-[10px] px-[20px] text-[#272343]">
+        <div className="flex flex-row justify-between items-center px-60 p-5 border-b-[1px] text-[14px] font-medium">
+          <button className="flex flex-row justify-center items-center gap-5 border-[1px] border-[#E1E3E5] rounded-[4px] py-[10px] px-[20px] text-[#272343] hover:scale-95 transition-all hover:bg-[#F48E28] hover:text-white">
             <GiHamburgerMenu />
-            <button>All Categories</button>
-          </div>
-          <div className="flex flex-row justify-center items-center font-[8px] gap-10 text-[#8B8BA5]">
-            <button>Home</button>
-            <button>Menu</button>
-            <button>Service</button>
-            <button>Testimonial</button>
+            <p>All Categories</p>
+          </button>
+          <div className="flex flex-row justify-center items-center gap-10 text-[#8B8BA5]">
+            <button className="hover:scale-95 transition-all hover:text-[#F48E28]">
+              Home
+            </button>
+            <button className="hover:scale-95 transition-all hover:text-[#F48E28]">
+              Menu
+            </button>
+            <button className="hover:scale-95 transition-all hover:text-[#F48E28]">
+              Service
+            </button>
+            <button className="hover:scale-95 transition-all hover:text-[#F48E28]">
+              Testimonial
+            </button>
           </div>
           <div className="flex flex-row justify-center items-center gap-5 text-[#8B8BA5]">
             <p>Contact:</p>
@@ -68,20 +79,61 @@ function App() {
         <div className="flex flex-row justify-between items-center px-60">
           <img src="/images/main-burger.svg" />
           <div className="flex flex-1 flex-col justify-start items-start text-left pl-48">
-            <p className="text-[14px] font-semibold text-[#F48E28]">
+            <p className="text-[14px] font-semibold text-[#F48E28] pb-2">
               TRY OUR BEST LIMITED EDITION!
             </p>
-            <p className="text-[45px] font-bold text-[#272343]">
+            <p className="text-[45px]/[50px] font-bold text-[#272343] pb-6">
               DOUBLE CHEESE BURGER
             </p>
-            <p className="text-[14px] font-normal text-[#68707D] w-[500px]">
+            <p className="text-[14px] font-normal text-[#68707D] w-[515px] pb-6">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Commodo,
               sed proin amet a vestibulum enim volutpat lacus. Volutpat arcu sit
               sed tortor etiam volutpat ipsum.{" "}
             </p>
-            <p className="text-[30px] font-bold text-[#272343]">$18.00</p>
-            <p className="text-[20px] font-bold text-[#B7BCC8]">$25.00</p>
+            <p className="text-[25px] font-bold text-[#272343]">$18.00</p>
+            <p className="text-[15px] line-through font-medium text-[#B7BCC8]">
+              $25.00
+            </p>
+
+            <div className="flex flex-row justify-center items-center gap-20 pt-10">
+              <div className="flex flex-row justify-center items-center gap-5 bg-[#F2F2F2] w-[134px] h-[48px] rounded-[48px]">
+                <button className="bg-[#F48E28] px-[10px] py-[10px] rounded-full text-white hover:scale-95 transition-all">
+                  <FaPlus />
+                </button>
+
+                <p className="text-[20px] font-bold">1</p>
+                <button className="bg-[#F48E28] px-[10px] py-[10px] rounded-full text-white hover:scale-95 transition-all">
+                  <FaMinus />
+                </button>
+              </div>
+
+              <button className="flex flex-row justify-center items-center bg-[#F48E28] text-white text-[16px] font-semibold w-[285px] h-[50px] rounded-[10px] gap-4 hover:scale-95 transition-all border-2 border-[#F48E28] hover:bg-white hover:border-[#F48E28] hover:text-[#F48E28]">
+                <FaBasketShopping size={20} />
+                <p> Add to cart</p>
+              </button>
+            </div>
           </div>
+        </div>
+
+        {/*Menu*/}
+        <div className="flex flex-row justify-center items-center ps-60 w-[900px] gap-10">
+          <button className="flex flex-1 flex-col justify-center items-start text-left bg-white w-[130px] h-[138px] rounded-[30px] drop-shadow-[0_10px_10px_rgba(0,0,0,0.1)] pl-4">
+            <p className="text-[12px] font-bold">Cake</p>
+            <p className="text-[10px] font-normal">Chocolate</p>
+            <p className="text-[10px] font-bold">$18.00</p>
+          </button>
+
+          <button className="flex flex-1 flex-col justify-center items-start text-left bg-white w-[130px] h-[138px] rounded-[30px] drop-shadow-[0_10px_10px_rgba(0,0,0,0.1)] pl-4">
+            <p className="text-[12px] font-bold">Cake</p>
+            <p className="text-[10px] font-normal">Chocolate</p>
+            <p className="text-[10px] font-bold">$18.00</p>
+          </button>
+
+          <button className="flex flex-1 flex-col justify-center items-start text-left bg-white w-[130px] h-[138px] rounded-[30px] drop-shadow-[0_10px_10px_rgba(0,0,0,0.1)] pl-4">
+            <p className="text-[12px] font-bold">Cake</p>
+            <p className="text-[10px] font-normal">Chocolate</p>
+            <p className="text-[10px] font-bold">$18.00</p>
+          </button>
         </div>
       </div>
     </>
